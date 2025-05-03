0f4c9a63fa0874e7c25641f18b787ca7b6afb4c3
AshleeHODOR <hodor1987@yahoo.com>
2025-05-03 14:55:31 -0700

    106-init function

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,17 @@
-
-function sayhello() {
-    console.log("Hello World");
+function saveTask()
+{
+    console.log("button clicked")
 }
 
-function init(){
-    console.log("hello im the init function");
-    sayhello();
+function init() {
+    //load data
+
+    //hook events
+
+    $("#btnSave").click(saveTask);
 }
 
-//specifiy why I am running the init function...
 
+//Why I am running the init function...
 window.onload = init; //this will run when poage is loaded - 
 // when the html and the css are loaded the logic will run
-//random comment
-
-
-
